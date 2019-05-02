6704c70aed15bc699118b96c24ea28a1779e5b56
Dongdong Zhang <45675253+DZhangCoder@users.noreply.github.com>
2019-05-02 13:38:52 -0700

    updating liri.js with more functions

--- liri.js
+++ liri.js
@@ -10,6 +10,9 @@ var fs = require("fs");
 //moment
 var moment = require("moment");
 
+//spotify
+var Spotify = require('node-spotify-api');
+
 //add the code required to import the keys.js file and store it in a variable.
 var keys = require("./keys.js");
 
@@ -24,7 +27,7 @@ switch (action) {
     concert();
     break;
   case "spotify-this-song":
-    spotify();
+    spotifyMusic();
     break;
   case "movie-this":
     movie();
@@ -50,4 +53,80 @@ function concert() {
     });
 };
 
+//function spotifyMusic 
+function spotifyMusic() {
+  var spotify = new Spotify({
+    id: keys.spotify.id,
+    secret: keys.spotify.secret
+  });
 
+  spotify
+    .search({ type: 'track', query: value, limit: 2 })
+    .then(function (response) {
+      // console.log(response);
+      console.log("\n-----------------");
+      var data = response.tracks.items[0];
+      console.log("Artist: " + data.album.artists[0].name +
+        "\nThe Name of the Song: " + data.album.name +
+        "\nPreview Link: " + data.external_urls.spotify +
+        "\nAlbum: " + data.album.name);
+      console.log("-----------------")
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
+};
+
+debugger;
+
+// function movie() case "movie-this"
+function movie() {
+  axios.get(" https://www.omdbapi.com/?apikey=604e2704&t=" + value)
+    .then(function (response) {
+      var data = response.data;
+      // console.log(data); 
+      console.log("\n-----------------")
+      console.log(
+        "Title of the movie: " + data.Title
+        + "\nYear the movie came out: " + data.Year
+        + "\nIMDB Rating of the movie: " + data.imdbRating
+        + "\nRotten Tomatoes Rating of the movie: " + data.Ratings[1].Value
+        + "\nCountry where the movie was produced: " + data.Country
+        + "\nLanguage of the movie: " + data.Language
+        + "\n Plot of the movie: " + data.Plot
+        + "\nActors in the movie: " + data.Actors
+      );
+      console.log("-----------------")
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+};
+
+// case "do-what-it-says"
+function what() {
+  fs.readFile("random.txt","UTF8",function(err,data){
+    if (err){
+      return console.log(err)
+    }
+    // console.log(data); 
+    var dataArr = data.split(",");
+    // console.log(dataArr); 
+    action = dataArr[0]; 
+    value = dataArr[1];
+    switch (action) {
+      case "concert-this":
+        concert();
+        break;
+      case "spotify-this-song":
+        spotifyMusic();
+        break;
+      case "movie-this":
+        movie();
+        break;
+      case "do-what-it-says":
+        what();
+        break;
+    };
+  })
+};
